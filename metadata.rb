ef277be2f9842b9db4ab328cd22cc2b8960a4dd6
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-11-24 06:28:18 +0900

    clarify license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "keyboard"
 maintainer       "Ryuzee"
 maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
+license          "MIT"
 description      "Installs/Configures keyboard"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
